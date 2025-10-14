26c1033b7c59ae115957125b96e41d47b84c194e
Mauricio Baquero <mauricio@baquero.llc>
2025-10-14 15:08:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8644216719-abe61",
+  "appId": "1:768127796745:web:36d614c4402389bfe3dad8",
+  "apiKey": "AIzaSyCAYIEYLj-E5zw2D-wmiPq_7Ev09XJ-Ssg",
+  "authDomain": "studio-8644216719-abe61.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "768127796745"
+};
