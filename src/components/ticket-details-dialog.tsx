fba269386278eac1fc0a446c1508641d12c5bafd
Mauricio Baquero <mauricio@baquero.llc>
2026-02-19 16:42:55 +0000

    9523-525620188114d2d4.js:1 Uncaught ReferenceError: AlertDialogTrigger i

--- src/components/ticket-details-dialog.tsx
+++ src/components/ticket-details-dialog.tsx
@@ -45,6 +45,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { ImageViewerDialog } from './image-viewer-dialog';
 import { format } from 'date-fns';
